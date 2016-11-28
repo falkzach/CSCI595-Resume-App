d41ae3de4d136c1b6287c4a1bf438b4ab3799a31
Zachary Falkner <falkzach@gmail.com>
2016-11-28 12:08:18 -0700

    laravel base

--- src/backend/routes/web.php
+++ src/backend/routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+ROute::get('/phpinfo', function() {
+    return phpinfo();
+});
