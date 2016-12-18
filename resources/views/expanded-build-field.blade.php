faa16c7ee44171edbdbdfe293024fa5ca606cf7c
Payton Pietron <pietron@Paytons-iMac.local>
2016-12-17 20:34:17 -0700

    Making build page function to add info.

--- resources/views/expanded-build-field.blade.php
+++ resources/views/expanded-build-field.blade.php
@@ -10,7 +10,15 @@
   <div class="column1">
     <div class="col-sm-4-resume2">
       <div class="panel panel-info"> <div class="panel-heading"> <a class="X"> X </a> </div> </div>
-      <p class="template"> This is an example item. </p>
+
+      <div class="container-fluid" id="build-info-static" style="display:initial">
+        <dl>
+          <dt>Information:&nbsp;</dt>
+          <dd><span id="info_static">info</span></dd>
+        </dl>
+        <button class="btn build-button"> Edit </button>
+      </div>
+
     </div>
   </div>
 
@@ -18,18 +26,91 @@
     <div class="col-sm-4-resume2">
       <div class="center">
         <div class="panel panel-info"> <div class="panel-heading"> Add Items </div> </div>
+        <form>
+          <dl>
+            <dt><label for="info_input">Info:</label></dt>
+            <dd><input type="text" name="info" id="info_input" value=""/></dd>
+          </dl>
+          <br>
+          <button class="btn submit-button"> Add </button>
+        </form>
       </div>
-      <form>
-        <div class="form-group">
-          <label for="formGroupExampleInput"> Information </label>
-          <input type="textarea" class="form-control" id="formGroupExampleInput" placeholder="Example: University of Montana">
-        </div>
-        <div class="center"> <button type="button" class="btn btn-outline-primary"> Add </button> </div>
-      </form>
     </div>
   </div>
 
 </div>
 
 </div>
+@endsection
+
+@section('javascript')
+<script>
+    // changes from displaying build information to editing build information
+    // build object used to return build information from ajax calls
+    // TODO: implement change password
+    var build = {
+        CSRF_TOKEN: undefined,
+        id: NaN,
+        info: '',
+        init: function() {
+            build.CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+            build.getInfo();
+
+            $(".build-button").click(function(){
+                $("#build-info-static").css('display','none');
+                $("#build-info-edit").css('display','initial');
+            });
+
+            $(".submit-button").click(function(e){
+              console.log(e);
+                e.preventDefault();
+                build.updateUser();
+                $("#build-info-static").css('display','initial');
+                $("#build-info-edit").css('display','none');
+            });
+        },
+        getInfo: function() {
+            $.ajax({
+                url: 'http://localhost:8000/api/school/',
+                data: {_token: build.CSRF_TOKEN},
+                dataType: 'JSON',
+                success: function (data) {
+                  console.log(data);
+                    build.updatePage(data);
+                }
+            });
+        },
+        updateUser: function() {
+            var data = {
+                _token: build.CSRF_TOKEN,
+                info: $('#info_input').val(),
+            };
+            console.log(data);
+
+            $.ajax({
+                type: "POST",
+                url: '/api/school/update/2',
+                data: data,
+                dataType: 'JSON',
+                success: function (data) {
+                    build.updatePage(data);
+                }
+            });
+        },
+
+        //updates values on build page
+        updatePage: function(data) {
+            console.log(data);
+            build.id = data['user']['id'];
+            build.info = data['user']['info'];
+            $('#info_static').text(build.info);
+            $('#info_input').val(build.info);
+        }
+    };
+
+    $(function() {
+        build.init();
+    });
+
+</script>
 @endsection
